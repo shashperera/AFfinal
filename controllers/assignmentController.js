f26e8abc21f1a026c7e731bcb7b0bcb72c3ef50b
Rakishi <shashipraba.perera@yahoo.com>
2019-06-21 00:23:59 +0530

    2nd

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -2,9 +2,21 @@
 
 const express = require('express');
 var router = express.Router();
+const mongoose= require('mongoose');
+const Assignment = mongoose.model('Assignment');
 
 router.get('/',(req,res)=>{
-    res.json('sample text');
+    res.render("assignments/add&edit", {
+        viewTitle : "Insert Assignments"
+    });
 });
+
+router.post('/',(req,res)=>{
+insertRecord(req,res)
+});
+
+function insertRecord(req,res){
+
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,21 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
 
 const assignemntController = require('./controllers/assignmentController');
 
 var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
-//conig view directory
-app.set('views',path.join(_dirname,'views'));
-app.engine();
+
+//config view directory
+app.use(bodyparser.json());
+app.set('views',path.join(__dirname,'/views/'));
+app.engine('hbs', exphbs({extname: 'hbs',defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts'}));
+app.set('view engine','hbs');
 
 app.listen(3000, () => {
     console.log('Express server started at port 3000');
